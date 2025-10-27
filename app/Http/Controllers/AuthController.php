36a0ebe697d42dd09989b6b77c65e49c08b406e0
jhordan.suzuki <jhordan.kendy1@gmail.com>
2025-10-27 02:15:58 -0300

    feat: implement form validations on the login page

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,6 +11,23 @@ class AuthController extends Controller
         return view('login');
     }
 
+    public function loginSubmit(Request $request)
+    {   
+        $request->validate([
+            'text_username' => 'required',
+            'text_password' => 'required',
+        ],
+        [
+            'text_username.required' => 'O campo usuário é obrigatório',
+            'text_password.required' => 'A senha é obrigatória',
+        ]);
+
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        dd($username, $password);
+    }
+
     public function logout(Request $request)
     {
         echo 'logout';
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::get('/login', [AuthController::class, 'login']);
+Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
 Route::get('/logout', [AuthController::class, 'logout']);
