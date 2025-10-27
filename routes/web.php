00856ee5a30b855f58d039220dcb596f7d3bac65
jhordan.suzuki <jhordan.kendy1@gmail.com>
2025-10-26 22:41:28 -0300

    feat: add screen login

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MainController;
+use App\Http\Controllers\AuthController;
 
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
