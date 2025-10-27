4f90dfb10147d835fe5d68a7b4a7b9cb6d80853b
jhordan.suzuki <jhordan.kendy1@gmail.com>
2025-10-26 22:22:52 -0300

    feat: clear project to build something

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,3 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
-Route::get('/', function () {
-    echo "Hello word";
-});
-
-Route::get('/main', [MainController::class, 'index']);
