e597959baa57ab1c11632ab157fc5aa146af04ac
jhordan.suzuki <jhordan.kendy1@gmail.com>
2025-10-27 15:05:53 -0300

    test: fix file to run unit test and feature test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,8 +12,10 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $responseEmpty = $this->get('/');
+        $response = $this->get('/login');
 
+        $responseEmpty->assertStatus(404);
         $response->assertStatus(200);
     }
 }
